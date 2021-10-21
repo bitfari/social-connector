961e4398f0c3911ab4c8931c044c54dddc7c2b41
Bitfari <85038940+bitfari@users.noreply.github.com>
2021-10-20 21:02:54 -0400

    Add files via upload

---
+++ @core/layouts/components/BuyNow.js
@@ -0,0 +1,12 @@
+// ** Third Party Components
+import { Button } from 'reactstrap'
+
+const BuyNow = () => {
+  return (
+    <div className='buy-now'>
+      </Button>
+    </div>
+  )
+}
+
+export default BuyNow
